f404429aff07624d89a207e5fb5f31aa226ca9c9
yuedaregone <810566249@qq.com>
2018-12-14 15:38:59 +0800

    poll

---
+++ src/utils/pool.h
@@ -0,0 +1,22 @@
+#ifndef __POOL_H__
+#define __POOL_H__
+#include <stdio.h>
+#include "array.h"
+
+typedef void* (*pool_create_item_fun)();
+typedef void (*pool_destroy_item_fun)(void* item);
+
+struct pool
+{
+    pool_create_item_fun create;
+    pool_destroy_item_fun destroy;
+    struct array* act;
+    struct array* dis;
+};
+
+struct pool* pool_create(int item_size, pool_create_item_fun create, pool_destroy_item_fun destroy);
+void pool_destroy(struct pool* p);
+void* pool_request(struct pool* p);
+void pool_return(struct pool* p, void* item);
+
+#endif
