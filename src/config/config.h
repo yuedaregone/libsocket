ab2455ac30a62ace34438ab0deb8a000aa258754
yuedaregone <810566249@qq.com>
2018-12-22 18:14:12 +0800

    config parse

--- src/config/config.h
+++ src/config/config.h
@@ -21,7 +21,7 @@ struct config
 };
 
 struct config* config_create();
-int config_load_local_data(const char* filename);
+int config_load_local_data(struct config* cfg, const char* filename);
 
 #ifndef CONFIG_IMPLEMENT
 #include "array.h"
@@ -31,14 +31,44 @@ struct config* config_create()
     struct config* cfg = (struct config*)malloc(sizeof(struct config));
     cfg->cfg_items = array_create(sizeof(struct config_item*));
     cfg->str_data = NULL;
+    return cfg;
 }
 
-int config_load_local_data(const char* filename)
+int config_load_local_data(struct config* cfg, const char* filename)
 {
-    
+    FILE* fp = fopen(filename, "rb");
+    if (fp == NULL) 
+        return -1;
+
+    fseek(fp, 0, SEEK_END);
+    long size = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+
+    cfg->str_data = (char*)malloc((size_t)size);
+    fread(cfg->str_data, (size_t)size, 1, fp);
+
+    char* ctx = cfg->str_data;
+    long index = 0;
+    while (index < size)
+    {
+        char ch = *(ctx + index);
+        switch (ch)
+        {
+            case '=':
+
+                break;
+            case '\n':
+                *(ctx + index) = '\0';
+                if (index > 0 && *(ctx + index - 1) == '\r')
+                     *(ctx + index - 1) = '\0';
+                
+                break;
+        }
+    }
+    return 0;
 }
 
 
-#endif
+#endif //CONFIG_IMPLEMENT
 
 #endif //__CONFIG_H__
